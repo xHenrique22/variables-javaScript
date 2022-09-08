af643a3861d969fb45fa58612ce6438b778cb1d0
xHenrique22 <henrique.oliveira9823@gmail.com>
2022-09-08 16:54:15 -0300

    Atividade 1

---
+++ saude.js
@@ -0,0 +1,14 @@
+/*
+ Crie um programa de saúde que trabalhe com variáveis que recebam dados de peso,
+ nome, altura, data de internação e uma variável que receba o dado se o paciente é diabético ou não.
+*/
+
+const prompt = require('prompt-sync')();
+
+const peso = prompt('Qual o seu peso: ')
+const nome = prompt('Qual seu nome: ')
+const altura = prompt('Qual a sua altura: ')
+const data = prompt('Data de internação: ')
+const diabetico = prompt('Diabético - Digite 1 para sim e 2 para não: ')
+
+
